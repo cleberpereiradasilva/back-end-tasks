8b571e2b7c30b4500a48a40f2ef6a04a6d8aa95f
Cleber Silva <kernel.conf@gmail.com>
2020-04-11 03:16:14 -0300

    feat: set email to lowercase, return object on craate task

--- api/task.js
+++ api/task.js
@@ -1,63 +1,71 @@
-const moment = require('moment')
-module.exports = app => {
-	const getTasks = (req, res) => {
-		const date = req.query.date ? req.query.date : moment().endOf('day').toDate()
+const moment = require("moment");
+module.exports = (app) => {
+  const getTasks = (req, res) => {
+    const date = req.query.date
+      ? req.query.date
+      : moment().endOf("day").toDate();
 
-		app.db('tasks')
-			.where({ userId: req.user.id })
-			.where('estimatedAt', '<=', date)
-			.then(tasks => res.status(200).json(tasks))
-			.catch(err => res.status(500).json(err))
-	}
+    app
+      .db("tasks")
+      .where({ userId: req.user.id })
+      .where("estimatedAt", "<=", date)
+      .orderBy(["id"])
+      .then((tasks) => res.status(200).json(tasks))
+      .catch((err) => res.status(500).json(err));
+  };
 
-	const save = (req, res) => {
-		if (!req.body.desc.trim()) {
-			return res.status(400).send('Descrição do campo obrigatório!')
-		}
-		req.body.userId = req.user.id
-		app.db('tasks')
-			.insert(req.body)
-			.then(_ => res.status(204).send())
-			.catch(err => res.status(500).json(err))
-	}
+  const save = (req, res) => {
+    console.log("Add Task");
+    console.log(req.body);
+    if (!req.body.desc.trim()) {
+      return res.status(400).send("Descrição do campo obrigatório!");
+    }
+    req.body.userId = req.user.id;
+    app
+      .db("tasks")
+      .insert(req.body)
+      .returning("*")
+      .then((data) => res.status(200).send(data[0]))
+      .catch((err) => res.status(500).json(err));
+  };
 
+  const remove = (req, res) => {
+    app
+      .db("tasks")
+      .where({ id: req.params.id, userId: req.user.id })
+      .del()
+      .then((rowDeleted) => {
+        if (rowDeleted > 0) {
+          res.status(204).send();
+        } else {
+          res.status(400).send("Registro não encontrado!");
+        }
+      })
+      .catch((err) => res.status(500).json(err));
+  };
 
-	const remove = (req, res) => {
-		app.db('tasks')
-			.where({ id: req.params.id, userId: req.user.id })
-			.del()
-			.then(rowDeleted => {
-				if (rowDeleted > 0) {
-					res.status(204).send()
-				} else {
-					res.status(400).send("Registro não encontrado!")
-				}
-			})
-			.catch(err => res.status(500).json(err))
-	}
+  const doneTask = (req, res, doneAt) => {
+    app
+      .db("tasks")
+      .where({ id: req.params.id, userId: req.user.id })
+      .update({ doneAt })
+      .then((tasks) => res.status(204).json(tasks))
+      .catch((err) => res.status(500).json(err));
+  };
 
-
-	const doneTask = (req, res, doneAt) => {
-		app.db('tasks')
-			.where({ id: req.params.id, userId: req.user.id })
-			.update({ doneAt })
-			.then(tasks => res.status(204).json(tasks))
-			.catch(err => res.status(500).json(err))
-	}
-
-
-	const toggleTask = (req, res) => {
-		app.db('tasks')
-			.where({ id: req.params.id, userId: req.user.id })
-			.first()
-			.then(task => {
-				if (!task) {
-					return res.status(400).send("Registro não encontrado!")
-				}
-				const doneAt = task.doneAt ? null : new Date()
-				return doneTask(req, res, doneAt)
-			})
-			.catch(err => res.status(500).json(err))
-	}
-	return { getTasks, save, remove, toggleTask }
-}
+  const toggleTask = (req, res) => {
+    app
+      .db("tasks")
+      .where({ id: req.params.id, userId: req.user.id })
+      .first()
+      .then((task) => {
+        if (!task) {
+          return res.status(400).send("Registro não encontrado!");
+        }
+        const doneAt = task.doneAt ? null : new Date();
+        return doneTask(req, res, doneAt);
+      })
+      .catch((err) => res.status(500).json(err));
+  };
+  return { getTasks, save, remove, toggleTask };
+};
--- api/user.js
+++ api/user.js
@@ -1,27 +1,28 @@
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require("bcrypt-nodejs");
 
-module.exports = app => {
-	const getHash = (password, callback) => {
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(password, salt, null, (err, hash) => callback(hash))
-		})
-	}
+module.exports = (app) => {
+  const getHash = (password, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, null, (err, hash) => callback(hash));
+    });
+  };
 
-	const save = (req, res) => {
-		getHash(req.body.password, hash => {
-			const password = hash
-			console.log(JSON.stringify(req.body))
-			console.log(password)
-			app.db('users').insert({
-				name: req.body.name,
-				email: req.body.email,
-				password: password
-			})
-				.then(_ => res.status(204).send())
-				.catch(err => res.status(500).json(err))
+  const save = (req, res) => {
+    getHash(req.body.password, (hash) => {
+      const password = hash;
+      console.log(JSON.stringify(req.body));
+      console.log(password);
+      app
+        .db("users")
+        .insert({
+          name: req.body.name,
+          email: req.body.email.toLowerCase(),
+          password: password,
+        })
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(500).json(err));
+    });
+  };
 
-		})
-	}
-
-	return { save }
-}
+  return { save };
+};
